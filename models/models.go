d2e4f1a37239725ef72011ac5c351e0829f2d22d
lamvng <lam.nguyenv11@gmail.com>
2024-07-18 20:14:49 +0200

    feat: WIP - DB schemas

--- models/models.go
+++ models/models.go
@@ -1,1 +1,32 @@
 package models
+
+import (
+	"gorm.io/gorm"
+)
+
+type TransactionGeneralType struct {
+	gorm.Model
+	Name        string `gorm:"uniqueIndex"`
+	Description string
+	Categories  []TransactionCategory
+}
+
+type TransactionCategory struct {
+	gorm.Model
+	Name          string `gorm:"index"`
+	Description   string
+	SubCategories []TransactionSubCategory
+}
+
+type TransactionSubCategory struct {
+	gorm.Model
+	Name        string `gorm:"index"`
+	Description string
+}
+
+type User struct {
+	gorm.Model
+	FirstName string
+	LastName  string
+	Email     string `gorm:"uniqueIndex"`
+}
