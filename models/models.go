b8158226e377cdc9a3e81ecbd26c27ae4ce252c9
lamvng <lam.nguyenv11@gmail.com>
2024-07-19 23:50:07 +0200

    feat(db-schema): WIP - Add mroe schemas

--- models/models.go
+++ models/models.go
@@ -4,24 +4,66 @@ import (
 	"gorm.io/gorm"
 )
 
-type TransactionGeneralType struct {
+// Note: I name the object "Operation" instead of "Transaction"
+// To not confuse with DB Transaction.
+
+// Type of assets
+// Fixed by program. Eg: Liquid, commodities, real estate, bonds, stocks
+type AssetType struct {
 	gorm.Model
 	Name        string `gorm:"uniqueIndex"`
 	Description string
-	Categories  []TransactionCategory
 }
 
-type TransactionCategory struct {
+// Type of user accounts
+// Eg. Everyday account, saving bonds, stock...
+type AccountType struct {
+	gorm.Model
+	Name        string `gorm:"uniqueIndex"`
+	Description string
+}
+
+// User account
+type Account struct {
+	gorm.Model
+	Name        string `gorm:"uniqueIndex"`
+	Description string
+	User        User
+	Type        AccountType
+}
+
+// General type of an operation
+// Eg. Buying (groceries), selling (stocks)...
+type OperationGeneralType struct {
+	gorm.Model
+	Name        string `gorm:"uniqueIndex"`
+	Description string
+	Categories  []OperationCategory
+	AccountType AccountType
+}
+
+// Operation category
+// Eg. Healthcare, shopping, holidays
+type OperationCategory struct {
 	gorm.Model
 	Name          string `gorm:"index"`
 	Description   string
-	SubCategories []TransactionSubCategory
+	SubCategories []OperationSubCategory
 }
 
-type TransactionSubCategory struct {
+// Subcategory
+// Detailed action, eg: groceries
+type OperationSubCategory struct {
 	gorm.Model
 	Name        string `gorm:"index"`
 	Description string
+}
+
+// Operation detail
+type Operation struct {
+	gorm.Model
+	Account     Account
+	SubCategory OperationSubCategory
 }
 
 type User struct {
