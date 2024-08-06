a42984b77aea60613113a56e54aef221a5f2cff3
lamvng <lam.nguyenv11@gmail.com>
2024-08-06 18:00:46 +0200

    chore: User: Cahnge Json schema

--- models/users.go
+++ models/users.go
@@ -2,20 +2,20 @@ package models
 
 type User struct {
 	Base
-	FirstName             string                `json:"firstname"`
-	LastName              string                `json:"lastname"`
-	Username              string                `json:"username" gorm:"uniqueIndex"`
+	FirstName             string                `json:"firstName"`
+	LastName              string                `json:"lastName"`
+	Username              string                `json:"userName" gorm:"uniqueIndex"`
 	Email                 string                `json:"email"`
-	PasswordHash          string                `json:"passwordhash"`
+	PasswordHash          string                `json:"passwordHash"`
 	AssetAccounts         []*AssetAccount       `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
 	LiquidAccounts        []*LiquidAccount      `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
 	TransactionCategories []TransactionCategory `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
 }
 
 type CreateUserInput struct {
-	FirstName string `json:"firstname" binding:"required"`
-	LastName  string `json:"lastname" binding:"required"`
-	Username  string `json:"username" binding:"required"`
+	FirstName string `json:"firstName" binding:"required"`
+	LastName  string `json:"lastName" binding:"required"`
+	Username  string `json:"userName" binding:"required"`
 	Email     string `json:"email" binding:"required"`
 	Password  string `json:"password" binding:"required"`
 }
